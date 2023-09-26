9458ab636486f4aa6031c1803668c64aa48f8e42
Matheus Santos <matheusstss0279@gmail.com>
2023-09-26 20:01:25 -0300

    final home

--- application/views/home.php
+++ application/views/home.php
@@ -8,7 +8,13 @@
 		<a href="<?= base_url('login') ?>" target="_blank" rel="noopener noreferrer">Acesso Restrito</a>
 	</div>
 
-	<div class="m-2 container-fluid">
+	<div id="aviso" class="m-2 container-fluid d-none">
+		<div style="text-align: center;" class="alert alert-danger" role="alert">
+			Nenhum comunicado cadastrado no momento!
+		</div>
+	</div>
+
+	<div id="exibicao" class="m-2 container-fluid">
 		<div class="row">
 			<!-- Coluna 1: Imagem (50% da largura) -->
 			<div class="col-md-8" style="background-color: #f0f0f0;">
@@ -24,20 +30,21 @@
 						<label for="titulo" class="form-label">Título</label>
 						<input type="text" class="form-control" id="titulo" disabled value="">
 					</div>
-					<div class="mb-3">
+					<div id="div_descricao" class="mb-3">
 						<label for="descricao" class="form-label">Descrição</label>
 						<textarea class="form-control" id="descricao" disabled rows="4"></textarea>
 					</div>
 					<div class="mb-3">
-						<label for="link" class="form-label">Link</label>
-						<input type="text" class="form-control" id="link" disabled value="">
+						<a id="link" href="#" target="_blank" rel="noopener noreferrer"></a>
+						<!-- <label for="link" class="form-label">Link</label>
+						<input type="text" class="form-control" id="link" disabled value=""></input> -->
 					</div>
 				</form>
 			</div>
 		</div>
 		<div class="d-flex justify-content-between">
-			<button type="button" class="btn btn-primary m-1">'<-'</button>
-			<button type="button" class="btn btn-primary m-1">'->'</button>
+			<button id="anterior" type="button" class="btn btn-primary m-1"><i class="fa-solid fa-arrow-left"></i></button>
+			<button id="proximo" type="button" class="btn btn-primary m-1"><i class="fa-solid fa-arrow-right"></i></button>
 		</div>
 	</div>
 
@@ -49,6 +56,7 @@
 <script>
 	const base_url = '<?= base_url() ?>'
 	let comunicados = ''
+	let atual = 0
 	window.onload = () => carrega_comunicados();
 
 	async function carrega_comunicados() {
@@ -59,19 +67,74 @@
 			if (result.status == 'success') {
 				comunicados = result.data
 				document.querySelector('#titulo').value = comunicados[0].titulo
-				document.querySelector('#descricao').value = comunicados[0].descricao
-				document.querySelector('#link').value = comunicados[0].link
-				document.querySelector('#imagem').src = base_url+comunicados[0].diretorio
+				if (comunicados[0].descricao == '') {
+					document.querySelector('#div_descricao').classList.add('d-none')
+				} else {
+					document.querySelector('#div_descricao').classList.remove('d-none')
+					document.querySelector('#descricao').value = comunicados[0].descricao
+				}
+				document.querySelector('#link').href = comunicados[0].link
+				document.querySelector('#link').textContent = comunicados[0].link
+				document.querySelector('#imagem').src = base_url + comunicados[0].diretorio
 			} else {
-
+				document.querySelector('#exibicao').classList.add('d-none')
+				document.querySelector('#aviso').classList.remove('d-none')
 			}
 		} catch (error) {
 			console.log(error)
 		}
 	}
-	setTimeout(() => {
-		//console.log("Delayed for 1 second.");
-		console.log(comunicados)
-	}, "1000");
 
+	document.querySelector('#proximo').addEventListener('click', () => {
+		adiciona_proximo()
+	})
+
+	function adiciona_proximo() {
+
+		if ((comunicados.length - 1) == atual) {
+			atual = 0
+		} else {
+			atual++
+		}
+		document.querySelector('#titulo').value = comunicados[atual].titulo
+
+		if (comunicados[atual].descricao == '') {
+			document.querySelector('#div_descricao').classList.add('d-none')
+		} else {
+			document.querySelector('#div_descricao').classList.remove('d-none')
+			document.querySelector('#descricao').value = comunicados[atual].descricao
+		}
+
+		document.querySelector('#link').href = comunicados[atual].link
+		document.querySelector('#link').textContent = comunicados[atual].link
+		document.querySelector('#imagem').src = base_url + comunicados[atual].diretorio
+	}
+
+	document.querySelector('#anterior').addEventListener('click', () => {
+		adiciona_anterior()
+	})
+
+	function adiciona_anterior() {
+
+		if (atual == 0) {
+			atual = comunicados.length - 1
+		} else {
+			atual--
+		}
+
+		document.querySelector('#titulo').value = comunicados[atual].titulo
+		
+		if (comunicados[atual].descricao == '') {
+			document.querySelector('#div_descricao').classList.add('d-none')
+		} else {
+			document.querySelector('#div_descricao').classList.remove('d-none')
+			document.querySelector('#descricao').value = comunicados[atual].descricao
+		}
+
+		document.querySelector('#link').href = comunicados[atual].link
+		document.querySelector('#link').textContent = comunicados[atual].link
+		document.querySelector('#imagem').src = base_url + comunicados[atual].diretorio
+	}
+
+	setInterval(adiciona_proximo, 15000);
 </script>
