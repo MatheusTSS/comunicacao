7201add3a81f131279db7cd435825d182ef8cce8
Matheus Santos <matheusstss0279@gmail.com>
2023-09-27 20:51:49 -0300

    parte 2 front-end

--- application/views/login/view_login.php
+++ application/views/login/view_login.php
@@ -7,15 +7,17 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
 
-	<link rel="stylesheet" href="<?= base_url('assets/login/fonts/icomoon/style.css')?>">
+	<link rel="stylesheet" href="<?= base_url('assets/login/fonts/icomoon/style.css') ?>">
 
-	<link rel="stylesheet" href="<?= base_url('assets/login/css/owl.carousel.min.css')?>">
+	<link rel="stylesheet" href="<?= base_url('assets/login/css/owl.carousel.min.css') ?>">
 
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="<?= base_url('assets/login/css/bootstrap.min.css')?>">
+	<link rel="stylesheet" href="<?= base_url('assets/login/css/bootstrap.min.css') ?>">
 
 	<!-- Style -->
-	<link rel="stylesheet" href="<?= base_url('assets/login/css/style.css')?>">
+	<link rel="stylesheet" href="<?= base_url('assets/login/css/style.css') ?>">
+
+	<script src="<?= base_url('assets/js/all.js') ?>"></script>
 
 	<style>
 		html,
@@ -34,13 +36,13 @@
 			background-color: #6c63ff;
 			color: #FFF;
 			width: 100%;
-			height: 8vh;
+			height: 6vh;
 			text-align: center;
-			line-height: 8vh;
+			line-height: 6vh;
 		}
 	</style>
 
-	<title>Login #7</title>
+	<title><?= APP_NAME ?></title>
 </head>
 
 <body>
@@ -51,50 +53,32 @@
 		<div class="container mb-5">
 			<div class="row">
 				<div class="col-md-6">
-					<img src="<?= base_url('assets/login/images/undraw_remotely_2j6y.svg')?>" alt="Image" class="img-fluid">
+					<img src="<?= base_url('assets/login/images/undraw_remotely_2j6y.svg') ?>" alt="Image" class="img-fluid">
 				</div>
 				<div class="col-md-6 contents">
 					<div class="row justify-content-center">
-						<div class="col-md-8">
+						<div class="col-md-8 mt-5">
 							<div class="mb-4">
-								<h3>Sign In</h3>
-								<p class="mb-4">Lorem ipsum dolor sit amet elit. Sapiente sit aut eos consectetur adipisicing.</p>
+								<h3>Entrar</h3>
+								<p class="mb-4">Para acessar o sistema, por favor, informe o usuário e a senha.</p>
 							</div>
-							<form action="#" method="post">
+							<?php get_flash_messages() ?>
+							<form action="<?= base_url('login/autentica') ?>" method="post">
 								<div class="form-group first">
-									<label for="username">Username</label>
-									<input type="text" class="form-control" id="username">
+									<label for="usuario">Usuário</label>
+									<input type="text" class="form-control" id="usuario" name="usuario" required placeholder="Insira seu usuário">
 
 								</div>
-								<div class="form-group last mb-4">
-									<label for="password">Password</label>
-									<input type="password" class="form-control" id="password">
+								<label for="senha">Senha</label>
+								<div class="form-group input-group last mb-4">
+									<input type="password" class="form-control" id="senha" name="senha" required placeholder="Insira sua senha">
+									<button class="btn btn-outline-primary" type="button" id="btn_senha"><i id="icon_senha" class="fa-solid fa-eye-slash"></i></button>
 
 								</div>
+								<hr>								
+								<input type="submit" value="Entrar" class="btn btn-block btn-primary">
+								
 
-								<div class="d-flex mb-5 align-items-center">
-									<label class="control control--checkbox mb-0"><span class="caption">Remember me</span>
-										<input type="checkbox" checked="checked" />
-										<div class="control__indicator"></div>
-									</label>
-									<span class="ml-auto"><a href="#" class="forgot-pass">Forgot Password</a></span>
-								</div>
-
-								<input type="submit" value="Log In" class="btn btn-block btn-primary">
-
-								<span class="d-block text-left my-4 text-muted">&mdash; or login with &mdash;</span>
-
-								<div class="social-login">
-									<a href="#" class="facebook">
-										<span class="icon-facebook mr-3"></span>
-									</a>
-									<a href="#" class="twitter">
-										<span class="icon-twitter mr-3"></span>
-									</a>
-									<a href="#" class="google">
-										<span class="icon-google mr-3"></span>
-									</a>
-								</div>
 							</form>
 						</div>
 					</div>
@@ -103,37 +87,36 @@
 
 			</div>
 		</div>
+		<br><br><br>
 		<footer>
-			<!-- Copyright -->
 			<div class="text-white mb-3 mb-md-0">
-				Copyright © 2020. All rights reserved.
+				Copyright &copy; <?= APP_NAME ?> <?= date('Y') ?>
 			</div>
-			<!-- Copyright -->
-
-			<!-- Right -->
-			<div>
-				<a href="#!" class="text-white me-4">
-					<i class="fab fa-facebook-f"></i>
-				</a>
-				<a href="#!" class="text-white me-4">
-					<i class="fab fa-twitter"></i>
-				</a>
-				<a href="#!" class="text-white me-4">
-					<i class="fab fa-google"></i>
-				</a>
-				<a href="#!" class="text-white">
-					<i class="fab fa-linkedin-in"></i>
-				</a>
-			</div>
-			<!-- Right -->
 		</footer>
 	</div>
 
 
-	<script src="<?= base_url('assets/login/js/jquery-3.3.1.min.js')?>"></script>
-	<script src="<?= base_url('assets/login/js/popper.min.js')?>"></script>
-	<script src="<?= base_url('assets/login/js/bootstrap.min.js')?>"></script>
-	<script src="<?= base_url('assets/login/js/main.js')?>"></script>
+	<script src="<?= base_url('assets/login/js/jquery-3.3.1.min.js') ?>"></script>
+	<script src="<?= base_url('assets/login/js/popper.min.js') ?>"></script>
+	<script src="<?= base_url('assets/login/js/bootstrap.min.js') ?>"></script>
+	<script src="<?= base_url('assets/login/js/main.js') ?>"></script>
+
+	<script>
+        const senhaInput = document.getElementById('senha');
+        const btnSenha = document.getElementById('btn_senha');
+        const eyeIcon = document.getElementById('icon_senha');
+
+        btnSenha.addEventListener('click', () => {
+            if (senhaInput.type === 'password') {
+                senhaInput.type = 'text';
+				btnSenha.innerHTML = '<i id="icon_senha" class="fa-solid fa-eye"></i>'
+            } else {
+                senhaInput.type = 'password';
+				btnSenha.innerHTML = '<i id="icon_senha" class="fa-solid fa-eye-slash"></i>'
+            }
+        });
+    </script>
+
 </body>
 
 </html>
--- application/views/usuarios/view_altera_senha.php
+++ application/views/usuarios/view_altera_senha.php
@@ -1,6 +1,6 @@
 <div class="container mt-5">
 	<h3 class="mt-3 mb-3">Alterar Senha</h3>
-	<form action="<?= base_url('usuario/altera_senha')?>" method="post">
+	<form action="<?= base_url('usuario/altera_senha') ?>" method="post">
 		<div class="row g-3">
 			<div class="col-md-6 mb-3">
 				<label for="usuario" class="form-label">Usuario</label>
@@ -9,15 +9,26 @@
 		</div>
 		<br>
 		<div class="row g-3">
-			<div class="col-md-6 mb-3">
+			<div id="div_nova_senha" class="col-md-6 mb-3">
 				<label for="nova_senha" class="form-label">Nova Senha</label>
-				<input type="password" class="form-control" id="nova_senha" name="nova_senha" required>
+				<div class="input-group">
+					<input type="password" class="form-control" id="nova_senha" name="nova_senha" required>
+					<button class="btn btn-outline-secondary" type="button" id="btn_senha"><i id="icon_senha" class="fa-solid fa-eye-slash"></i></button>
+				</div>
+				<div id="aviso_nova_senha"></div>
 			</div>
-			<div class="col-md-6 mb-3">
+			<div id="div_confirma_nova_senha" class="col-md-6 mb-3">
 				<label for="confirma_nova_senha" class="form-label">Confirma Nova Senha</label>
-				<input type="password" class="form-control" id="confirma_nova_senha" name="confirma_nova_senha" required>
+				<div class="input-group">
+					<input type="password" class="form-control" id="confirma_nova_senha" name="confirma_nova_senha" required>
+					<button class="btn btn-outline-secondary" type="button" id="btn_senha2"><i id="icon_senha2" class="fa-solid fa-eye-slash"></i></button>
+				</div>
+				<div id="aviso_confirma_nova_senha"></div>
 			</div>
 		</div>
+
+		
+		<br>
 		<div class="col-12">
 			<button type="submit" id="salvar" disabled class="btn btn-primary">Salvar</button>
 		</div>
@@ -28,9 +39,13 @@
 	const nova_senha = document.querySelector('#nova_senha')
 	const confirma_nova_senha = document.querySelector('#confirma_nova_senha')
 	const salvar = document.querySelector('#salvar')
+	const aviso_nova_senha = document.querySelector('#aviso_nova_senha')
+	const aviso_confirma_nova_senha = document.querySelector('#aviso_confirma_nova_senha')
+	const div_nova_senha = document.querySelector('#div_nova_senha')
+	const div_confirma_nova_senha = document.querySelector('#div_confirma_nova_senha')
 
 	nova_senha.addEventListener('keyup', () => {
-		
+
 		nova_senha.value = nova_senha.value.trim()
 
 		if (nova_senha.value.length > 5 && nova_senha.value.length < 21) {
@@ -58,4 +73,59 @@
 		}
 	})
 
+
+	const nova_senha_input = document.getElementById('nova_senha');
+	const btnSenha = document.getElementById('btn_senha');
+	const eyeIcon = document.getElementById('icon_senha');
+
+	btnSenha.addEventListener('click', () => {
+		if (nova_senha_input.type === 'password') {
+			nova_senha_input.type = 'text';
+			btnSenha.innerHTML = '<i id="icon_senha" class="fa-solid fa-eye"></i>'
+		} else {
+			nova_senha_input.type = 'password';
+			btnSenha.innerHTML = '<i id="icon_senha" class="fa-solid fa-eye-slash"></i>'
+		}
+	});
+
+	const confirma_nova_senha_input = document.getElementById('confirma_nova_senha');
+	const btnSenha2 = document.getElementById('btn_senha2');
+	const eyeIcon2 = document.getElementById('icon_senha2');
+
+	btnSenha2.addEventListener('click', () => {
+		if (confirma_nova_senha_input.type === 'password') {
+			confirma_nova_senha_input.type = 'text';
+			btnSenha2.innerHTML = '<i id="icon_senha" class="fa-solid fa-eye"></i>'
+		} else {
+			confirma_nova_senha_input.type = 'password';
+			btnSenha2.innerHTML = '<i id="icon_senha" class="fa-solid fa-eye-slash"></i>'
+		}
+	});
+
+	div_nova_senha.addEventListener('click', () => {
+		aviso_nova_senha.innerHTML = `		<br><ul>
+												<li>A senha deve conter pelo menos 1 caractere.</li>
+												<li>A senha deve conter pelo menos 1 número.</li>
+												<li>A senha deve ter entre 6 e 25 caracteres de comprimento.</li>
+												<li>As senhas devem ser iguais.</li>
+											</ul>`
+	})
+
+	div_nova_senha.addEventListener('mouseleave', () => {
+		aviso_nova_senha.innerHTML = ``
+	})
+
+	div_confirma_nova_senha.addEventListener('click', () => {
+		aviso_confirma_nova_senha.innerHTML = `		<br><ul>
+														<li>A senha deve conter pelo menos 1 caractere.</li>
+														<li>A senha deve conter pelo menos 1 número.</li>
+														<li>A senha deve ter entre 6 e 25 caracteres de comprimento.</li>
+														<li>As senhas devem ser iguais.</li>
+													</ul>`
+	})
+
+	div_confirma_nova_senha.addEventListener('mouseleave', () => {
+		aviso_confirma_nova_senha.innerHTML = ``
+	})
+	
 </script>
